1f7c9489da350dce298d452a177896a533aaf03b
Hải Vũ <haivu@Hais-MacBook-Pro.local>
2024-09-24 13:16:47 +0700

    Completed

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import path from 'path';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 app.use(express.json());
 app.use("/api/products", productRoutes)
@@ -14,11 +15,11 @@ app.use("/api/products", productRoutes)
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, "/frontend/dist")))
     app.get("*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "/frontend/dist"))
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     })
 }
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server started at http://localhost:3000');
+    console.log('Server started at http://localhost:' + PORT);
 });
